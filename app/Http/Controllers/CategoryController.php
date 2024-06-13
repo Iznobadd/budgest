b3a8858b4e6dd00f0a36f89f6eb86fa37d68f00d
Iznobad <brandon.cebi.pro@gmail.com>
2024-06-13 10:28:25 +0200

    Add request category and store category

--- app/Http/Controllers/CategoryController.php
+++ app/Http/Controllers/CategoryController.php
@@ -2,6 +2,8 @@
 
 namespace App\Http\Controllers;
 
+use App\Http\Requests\Category\CreateCategoryRequest;
+use App\Models\BudgetCategory;
 use Inertia\Inertia;
 
 class CategoryController extends Controller
@@ -10,7 +12,12 @@ class CategoryController extends Controller
         return Inertia::render('Dashboard/AddCategory');
     }
 
-    public function storeCategory() {
+    public function storeCategory(CreateCategoryRequest $request) {
+        $validatedData = $request->validated();
+        $validatedData['user_id'] = \Auth::user()->id;
 
+        BudgetCategory::create($validatedData);
+
+        return to_route('dashboard.overview')->with('success', 'The category has been added successfully');
     }
 }
